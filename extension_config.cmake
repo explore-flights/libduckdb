9a9e10c7c1ec85a3c55b31ae461285e0c0803321
Felix <23635466+its-felix@users.noreply.github.com>
2025-05-26 02:59:25 +0200

    update to httpfs s3 fix
    https://github.com/duckdb/duckdb/issues/17603

--- extension_config.cmake
+++ extension_config.cmake
@@ -4,7 +4,7 @@ duckdb_extension_load(json)
 duckdb_extension_load(icu)
 duckdb_extension_load(httpfs
     GIT_URL https://github.com/duckdb/duckdb-httpfs
-    GIT_TAG 217ec8e04f6ed419c866a6d2496aa15aace4382f
+    GIT_TAG 00a26970171fe6643078deb3da8d5d322bb9578c
     INCLUDE_DIR extension/httpfs/include
 )
 duckdb_extension_load(aws
